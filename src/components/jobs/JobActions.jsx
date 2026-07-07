56dfe56c7cac31af5606412aa5d8e629dc169086
Hemant Birla <birlahemant.hb.b19@gmail.com>
2026-07-07 20:56:13 +0530

    Build reusable job action buttons

--- src/components/jobs/JobActions.jsx
+++ src/components/jobs/JobActions.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
-
-const JobActions = () => {
+import { FiEye, FiEdit2, FiTrash2 } from "react-icons/fi";
+import "../../styles/jobs.css";
+const JobActions = ({
+  job,
+  onView,
+  onEdit,
+  onDelete,
+}) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="job-actions">
+      <button
+        type="button"
+        className="job-action-btn view"
+        title="View Job"
+        onClick={() => onView(job)}
+      >
+        <FiEye />
+      </button>
 
-export default JobActions
+      <button
+        type="button"
+        className="job-action-btn edit"
+        title="Edit Job"
+        onClick={() => onEdit(job)}
+      >
+        <FiEdit2 />
+      </button>
+
+      <button
+        type="button"
+        className="job-action-btn delete"
+        title="Delete Job"
+        onClick={() => onDelete(job)}
+      >
+        <FiTrash2 />
+      </button>
+    </div>
+  );
+};
+
+export default JobActions;
