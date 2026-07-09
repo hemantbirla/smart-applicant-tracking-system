94b4247d47bdeb936c5993a519b6d3a0403462e0
Hemant Birla <birlahemant.hb.b19@gmail.com>
2026-07-09 15:29:35 +0530

    Assemble admin dashboard layout

--- src/pages/AdminDashboard/AdminDashboard.jsx
+++ src/pages/AdminDashboard/AdminDashboard.jsx
@@ -2,11 +2,8 @@ import DashboardLayout from "../../layouts/DashboardLayout";
 
 import AdminHeader from "../../components/admin/AdminHeader";
 import AdminStatistics from "../../components/admin/AdminStatistics";
-
-import "../../styles/adminDashboard.css";
 import UserSummary from "../../components/admin/UserSummary";
 import RecruiterSummary from "../../components/admin/RecruiterSummary";
-import { recruiterSummary } from "../../constants/adminDashboardData";
 import JobSummary from "../../components/admin/JobSummary";
 import ApplicationSummary from "../../components/admin/ApplicationSummary";
 import RecentActivities from "../../components/admin/RecentActivities";
@@ -14,21 +11,42 @@ import TopCompanies from "../../components/admin/TopCompanies";
 import SystemStatus from "../../components/admin/SystemStatus";
 import QuickActions from "../../components/admin/QuickActions";
 
+import { recruiterSummary } from "../../constants/adminDashboardData";
+
+import "../../styles/adminDashboard.css";
+
 const AdminDashboard = () => {
   return (
     <DashboardLayout>
       <div className="admin-dashboard">
+        {/* Header */}
         <AdminHeader />
+
+        {/* Statistics */}
         <AdminStatistics />
-        <UserSummary />
-        <RecruiterSummary data={recruiterSummary} />
+
+        {/* User & Recruiter Summary */}
+        <div className="admin-two-column">
+          <UserSummary />
+          <RecruiterSummary data={recruiterSummary} />
+        </div>
+
+        {/* Job & Application Summary */}
         <div className="admin-two-column">
           <JobSummary />
           <ApplicationSummary />
         </div>
-        <RecentActivities />
-        <TopCompanies />
+
+        {/* Activities & Companies */}
+        <div className="admin-two-column">
+          <RecentActivities />
+          <TopCompanies />
+        </div>
+
+        {/* System Status */}
         <SystemStatus />
+
+        {/* Quick Actions */}
         <QuickActions />
       </div>
     </DashboardLayout>
