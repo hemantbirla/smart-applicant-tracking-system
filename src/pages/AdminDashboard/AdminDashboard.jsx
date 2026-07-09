0afc2af20443f89d7e68c5c847e4a0bfcd613a40
Hemant Birla <birlahemant.hb.b19@gmail.com>
2026-07-09 14:44:02 +0530

    Implement user summary component

--- src/pages/AdminDashboard/AdminDashboard.jsx
+++ src/pages/AdminDashboard/AdminDashboard.jsx
@@ -4,14 +4,15 @@ import AdminHeader from "../../components/admin/AdminHeader";
 import AdminStatistics from "../../components/admin/AdminStatistics";
 
 import "../../styles/adminDashboard.css";
+import UserSummary from "../../components/admin/UserSummary";
 
 const AdminDashboard = () => {
   return (
     <DashboardLayout>
       <div className="admin-dashboard">
         <AdminHeader />
-
         <AdminStatistics />
+        <UserSummary />
       </div>
     </DashboardLayout>
   );
